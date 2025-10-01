bd198d708580d9e1dadc13907e5a778c0f094ba8
Jinx1914 <121838263+Jinx1914@users.noreply.github.com>
2025-10-01 15:50:39 +0800

    Update Forticare Escalation (No Internal Notes).js

--- Forticare Escalation (No Internal Notes).js
+++ Forticare Escalation (No Internal Notes).js
@@ -2,7 +2,7 @@
 // @name           FortiCare Escalation
 // @author         Nico Angelo Abanes and Carnil Anthony De Lara
 // @namespace      http://userscripts.frval.fortinet-emea.com/
-// @version        v1.2
+// @version        v1.0
 // @description    Adds [SE]/[DM] to ticket title, triggers ticket submit, opens SharePoint, clears form textarea, shows banner only for escalated tickets.
 // @grant          none
 // @include        https://forticare.fortinet.com/CustomerSupport/SupportTeam/EditTicket.aspx*
@@ -159,6 +159,7 @@ $(document).ready(function () {
     }
   });
 });
+
 
 
 
